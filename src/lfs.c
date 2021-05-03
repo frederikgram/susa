761166945e8dcb930faaf593d3f8b2026e7b368d
bluetitanic <lasse@DESKTOP-GN9G943.localdomain>
2021-05-03 12:02:37 +0200

    kek

--- src/lfs.c
+++ src/lfs.c
@@ -4,28 +4,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+// Definitions for file system operations 
 int lfs_getattr( const char *, struct stat * );
 int lfs_readdir( const char *, void *, fuse_fill_dir_t, off_t, struct fuse_file_info * );
 int lfs_open( const char *, struct fuse_file_info * );
 int lfs_read( const char *, char *, size_t, off_t, struct fuse_file_info * );
 int lfs_release(const char *path, struct fuse_file_info *fi);
 
+// Defines operations for file operations to Fuse
 static struct fuse_operations lfs_oper = {
 	.getattr	= lfs_getattr,
 	.readdir	= lfs_readdir,
-	.mknod = NULL,
-	.mkdir = NULL,
-	.unlink = NULL,
-	.rmdir = NULL,
-	.truncate = NULL,
-	.open	= lfs_open,
-	.read	= lfs_read,
-	.release = lfs_release,
-	.write = NULL,
-	.rename = NULL,
-	.utime = NULL
+	.mknod      = NULL,
+	.mkdir      = NULL,
+	.unlink     = NULL,
+	.rmdir      = NULL,
+	.truncate   = NULL,
+	.open	    = lfs_open,
+	.read	    = lfs_read,
+	.release    = lfs_release,
+	.write      = NULL,
+	.rename     = NULL,
+	.utime      = NULL
 };
 
+
+// Prints the list of attributes of the file or of the files of the current directory
 int lfs_getattr( const char *path, struct stat *stbuf ) {
 	int res = 0;
 	printf("getattr: (path=%s)\n", path);
