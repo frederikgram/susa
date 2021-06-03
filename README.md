47d539a99ec9abeee60ba0acfa75534141f3e2f4
frederikgram <frederikxyz@hotmail.com>
2021-06-03 19:19:47 +0200

    Initial Push

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# dm510_assignment_3
-test
+# 
--- src/lfs.c
+++ src/lfs.c
@@ -4,23 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "lfs_controller.h"
+#include "structs.h"
+#include "hash.h"
+#include "search.h"
+
+struct HASHTABLE * fs_table;
 
 
-// Definitions for file system operations 
+// Definitions for file system opeunction ‘add_entry_to_table’:
 int lfs_getattr  ( const char *, struct stat * );
 int lfs_readdir  ( const char *, void *, fuse_fill_dir_t, off_t, struct fuse_file_info * );
 int lfs_open     ( const char *, struct fuse_file_info * );
 int lfs_read     ( const char *, char *, size_t, off_t, struct fuse_file_info * );
-int lfs_release  ( const char *path, struct fuse_file_info *fi);
+int lfs_release  ( const char *, struct fuse_file_info * );
 int lfs_mkdir    ( const char *, mode_t);
 int lfs_write    ( const char *, const char *, size_t, off_t, struct fuse_file_info *);
-int lfs_rmdir    ( const char *);
 int lfs_mknod    ( const char *, mode_t, dev_t);
-int lfs_truncate ( const char *, off_t);
-int lfs_utime    (const char * , const struct timespec ts[2]);
-int lfs_unlink   (const char *);
-int lfs_destroy  (void *);
 
 // Defines operations for file operations to Fuse
 static struct fuse_operations lfs_oper = {
@@ -28,206 +27,19 @@ static struct fuse_operations lfs_oper = {
 	.readdir	= lfs_readdir,
 	.mknod      = lfs_mknod,
 	.mkdir      = lfs_mkdir,
-	.rmdir      = lfs_rmdir,
-    .unlink     = lfs_unlink,
-	.truncate   = lfs_truncate,
-	.open	    = lfs_open,
-	.read	    = lfs_read,
-	.release    = lfs_release,
-	.write      = lfs_write,
-	.utime      = lfs_utime,
-    .destroy    = lfs_destroy
+	.rmdir      = NULL,
+    .unlink     = NULL,
+	.truncate   = NULL,
+	.open	    = NULL,
+	.read	    = NULL,
+	.release    = NULL,
+	.write      = NULL,
+	.utime      = NULL,
+    .destroy    = NULL,
 };
 
 
-/* Free the root directory on filesystem exit */
-int lfs_destroy(void * private_data) {
-    remove_directory(root_directory);
-}
-
-int lfs_unlink(const char * path) {
-    char * nonconst_path = strdup(path);
-    
-    #ifdef DEBUG
-        printf("unlink: Trying to unlink (remove) the file at path '%s'\n", path);
-    #endif
-    
-    /* Extract the name of the file to be created from the absolute path */
-    char * name = extract_last_segment(nonconst_path, '/');
-
-    if (name == NULL) {
-        fprintf(stderr, "unlink: Could not extract name from the given path '%s'\n", path);
-        free(nonconst_path);
-        return -EINVAL;
-    }
-
-    /* Extract the parents path and prepend the root path to it */
-    char * parent_segments = extract_init_segments(nonconst_path, '/');
-    char * parent_path = prepend_root(parent_segments);
-
-    free(nonconst_path);
-
-    // Find the parent lfs_directory instance 
-    struct lfs_directory * parent = find_directory(root_directory, parent_path);
-    free(parent_path);
-
-    if (parent == NULL) {
-        fprintf(stderr, "unlink: Could not find a parent folder for new entry at path '%s'\n", path);
-        return -ENOENT;    
-    } 
-    
-    struct lfs_file* file = find_file(parent, name);
-    if (file == NULL) {
-        return -ENOENT;
-    }        
-
-    remove_file(file);
-    
-    return 0;
-}
-
-int lfs_utime(const char * path, const struct timespec ts[2]) {
-    char * nonconst_path = strdup(path);
-    
-    /* Extract the name of the file to be created from the absolute path */
-    char * name = extract_last_segment(nonconst_path, '/');
-    if (name == NULL) {
-        fprintf(stderr, "utime: Could not extract name from the given path '%s'\n", path);
-        free(nonconst_path);
-        return -EINVAL;
-    }
-
-    #ifdef DEBUG
-        printf("utime: Trying to modify the access and modification times of the entry at path '%s' \
-               to the values: %ld, %ld respectively\n", path, ts[0].tv_sec, ts[1].tv_sec);
-    #endif
-
-    /* Extract the parents path and prepend the root path to it */
-    char * parent_segments = extract_init_segments(nonconst_path, '/');
-    char * parent_path = prepend_root(parent_segments);
-
-    free(nonconst_path);
-
-    // Find the parent lfs_directory instance 
-    struct lfs_directory * parent = find_directory(root_directory, parent_path);
-    free(parent_path);
-
-    if (parent == NULL) {
-        fprintf(stderr, "utime: Could not find a parent folder for new entry at path '%s'\n", path);
-        return -ENOENT;    
-    } 
-    
-    struct lfs_file* file = find_file(parent, name);
-    if (file == NULL) {
-        return -ENOENT;
-    }        
-
-    file->last_accessed = ts[0].tv_sec;
-    file->last_modified = ts[1].tv_sec;
-
-    return 0;
-    
-}
-
-int lfs_truncate(const char * path, off_t size) {
-    char * nonconst_path = strdup(path);
-    
-    /* Extract the name of the file to be created from the absolute path */
-    char * name = extract_last_segment(nonconst_path, '/');
-    if (name == NULL) {
-        fprintf(stderr, "truncate: Could not extract name from the given path '%s'\n", path);
-        free(nonconst_path);
-        return -EINVAL;
-    }
-
-    /* Extract the parents path and prepend the root path to it */
-    char * parent_segments = extract_init_segments(nonconst_path, '/');
-    char * parent_path = prepend_root(parent_segments);
-
-    free(nonconst_path);
-
-    // Find the parent lfs_directory instance 
-    struct lfs_directory * parent = find_directory(root_directory, parent_path);
-    free(parent_path);
-
-    if (parent == NULL) {
-        fprintf(stderr, "truncate: Could not find a parent folder for new entry at path '%s'\n", path);
-        return -ENOENT;    
-    } 
-    
-    struct lfs_file* file = find_file(parent, name);
-    if (file == NULL) {
-        return -ENOENT;
-    }        
-
-     
-    file->data = realloc(file->data, size);
-    file->size = size;
-    file->last_modified = file->last_accessed;
-
-    return 0;
-
-
-}
-
-
-int lfs_mknod(const char * path, mode_t mode, dev_t rdev) {
-
-    char * nonconst_path = strdup(path);
-    
-    /* Extract the name of the file to be created from the absolute path */
-    char * name = extract_last_segment(nonconst_path, '/');
-    if (name == NULL) {
-        fprintf(stderr, "mkdir: Could not extract name from the given path '%s'\n", path);
-        free(nonconst_path);
-        return -1;
-    }
-
-    /* Extract the parents path and prepend the root path to it */
-    char * parent_segments = extract_init_segments(nonconst_path, '/');
-    char * parent_path = prepend_root(parent_segments);
-
-    free(parent_segments);
-    free(nonconst_path);
-
-    // Find the parent lfs_directory instance 
-    struct lfs_directory * parent = find_directory(root_directory, parent_path);
-    free(parent_path);
-
-    if (parent == NULL) {
-        fprintf(stderr, "mknod: Could not find a parent folder for new entry at path '%s'\n", path);
-        return -ENOENT;
-    } 
-        
-    initialize_file(parent, name);
-    return 0;
-}
-
-int lfs_rmdir(const char * path) {
-
-
-    // @FLAG : weird
-    char * abs_path = (char *) malloc(6 + strlen(path));
-    strcpy(abs_path, "root");
-    strcat(abs_path, path);
-
-    struct lfs_directory * dir = find_directory(root_directory, abs_path);
-    free(abs_path);
-
-    if (dir == NULL) {
-        fprintf(stderr, "rmdir: Could not find any directory to remove at path '%s'\n", path);
-        return -ENOENT;
-    }
-
-    return remove_directory_if_empty(dir);
-}
-
 int lfs_write(const char * path, const char * buffer, size_t size, off_t offset, struct fuse_file_info * fi) {
-
-    #ifdef DEBUG
-        printf("write: Trying to write to %ld bytes to file at: '%s'\n", size, path);
-    #endif
-
     /* Fetch the pointer to the file, inserted into the
     fuse_file_info struct during lfs_open */
     struct lfs_file * file = (struct lfs_file *) fi->fh;
@@ -244,54 +56,81 @@ int lfs_write(const char * path, const char * buffer, size_t size, off_t offset,
         new_size = file->size + size - offset;
     }
 
-    #ifdef DEBUG
-        printf("write: Resizing file at path '%s' to be '%d' bytes long\n", path, new_size);
-    #endif
-
     file->data = realloc(file->data, new_size);
     memcpy(file->data + offset, buffer, size);
 
     file->size = new_size;
     file->last_modified = file->last_accessed;
     return size;
-
 }
 
 int lfs_mkdir(const char * path, mode_t mode) {
 
-    #ifdef DEBUG
-        printf("mkdir: Trying to create a directory with path: '%s'\n", path);
-    #endif
+    printf("mkdir: (path=%s)\n", path);
+
+    /* Extract the first n-1 and the nth segment from the given path */
+    char * path_dup = strdup(path);
+    char * last_segment = strrchr(path_dup, '/') + 1;
+    char * init_segments = (char *) malloc (strlen(path_dup) - strlen(last_segment));
+    strncat(init_segments, path_dup, (strlen(path_dup) - strlen(last_segment)) - 1);
+
+    /* if mkdir is trying to create a node in the root of the mount-point,
+    we manually specify the path of the parent to be "/",
+    otherwise we use n-1 segments from the given path, delimited by '/'
+    as the parent path */
+    char * parent_path = strcmp(init_segments, "") == 0 ? "/" : init_segments;
+    struct HASHTABLE_NODE * node = find_node_from_path(fs_table, parent_path);
     
-    char * nonconst_path = strdup(path);
+    /* No node was found at the parent path*/
+    if (node == NULL) { return -ENOENT; } 
     
-    /* Extract the name of the folder to be created from the absolute path */
-    char * name = extract_last_segment(nonconst_path, '/');
-    if (name == NULL) {
-        fprintf(stderr, "mkdir: Could not extract name of folder from the given path '%s'\n", path);
-        free(nonconst_path);
-        return -EINVAL;
+    /* The node at the parent path is not a directory, thus we cannot use it as a parent */
+    if (node->mode != 16877) {
+        return -ENOENT;
     }
+   
+    struct lfs_directory * parent = (struct lfs_directory * ) node->entry;
 
-    /* Extract the parents path and prepend the root path to it */
-    char * parent_segments = extract_init_segments(nonconst_path, '/');
-    char * parent_path = prepend_root(parent_segments);
-
-    free(nonconst_path);
-
-    // Find the parent lfs_directory instance 
-    struct lfs_directory * parent = find_directory(root_directory, parent_path);
-    free(parent_path);
-
-    if (parent == NULL) {
-        fprintf(stderr, "mkdir: Could not find a parent folder for the new directory at path '%s'\n", path);
-        return -ENOENT;    
-    } 
-        
-    initialize_directory(parent, name);
-
+    /* Initialize the new file and add it to the file system */
+    struct lfs_dir * dir = initialize_directory(parent, last_segment);
+    add_entry_to_table(fs_table, path_dup, (void *) dir);
     return 0;
 }
+
+int lfs_mknod(const char * path, mode_t mode, dev_t rdev) {
+
+    printf("mknod: (path=%s)\n", path);
+
+    /* Extract the first n-1 and the nth segment from the given path */
+    char * path_dup = strdup(path);
+    char * last_segment = strrchr(path_dup, '/') + 1;
+    char * init_segments = (char *) malloc (strlen(path_dup) - strlen(last_segment));
+    strncat(init_segments, path_dup, (strlen(path_dup) - strlen(last_segment)) - 1);
+
+    /* if mknod is trying to create a node in the root of the mount-point,
+    we manually specify the path of the parent to be "/",
+    otherwise we use n-1 segments from the given path, delimited by '/'
+    as the parent path */
+    char * parent_path = strcmp(init_segments, "") == 0 ? "/" : init_segments;
+    struct HASHTABLE_NODE * node = find_node_from_path(fs_table, parent_path);
+    
+    /* No node was found at the parent path*/
+    if (node == NULL) { return -ENOENT; } 
+    
+    /* The node at the parent path is not a directory, thus we cannot use it as a parent */
+    if (node->mode != 16877) {
+        return -ENOENT;
+    }
+   
+    struct lfs_directory * parent = (struct lfs_directory * ) node->entry;
+
+    /* Initialize the new file and add it to the file system */
+    struct lfs_file * file = initialize_file(parent, last_segment);
+    add_entry_to_table(fs_table, path_dup, (void *) file);
+    return 0;
+}
+
+
 
 // Prints the list of attributes of the file or of the files of the current directory
 int lfs_getattr( const char *path, struct stat *stbuf ) {
@@ -307,54 +146,32 @@ int lfs_getattr( const char *path, struct stat *stbuf ) {
         return res;
 	}
 
-    char * abs_path = (char *) malloc(6 + strlen(path));
-    strcpy(abs_path, "root");
-    strcat(abs_path, path);
+    struct HASHTABLE_NODE * node = find_node_from_path(fs_table, path);
+    
+    /* No node was found with the given path */
+    if (node == NULL) { return -ENOENT; } 
 
-
-    /* We're aware that instead of using the void pointer casting
-    that we've used here, we could just "copy paste" the code inside
-
-        > lfs_controller.h - find_file_or_directory
-
-    And avoid having to cast the pointers and compare
-    fields from unknown structures.  However, we've opted
-    to go for this approach, as wanted to utilize
-    void pointer casting, to learn a little more about how it
-    works and how we can interact with unknown structures */
-
-    void * found_struct = find_file_or_directory(abs_path);    
-    free(abs_path);
-
-    if (found_struct == NULL) {
-        fprintf(stderr, "getattr: Could not find an entry (file or directory) at path '%s'\n", path);
-        return -ENOENT;
-    }    
-
-    // Found a directory
-    if (((struct lfs_directory * )found_struct)->mode == 16877) {
-        struct lfs_directory * dir = ((struct lfs_directory * )found_struct);
-
+    /* Node is a directory */
+    if (node->mode == 16877) {
         stbuf->st_mode = 16877;
+        struct lfs_directory * dir = (struct lfs_directory *) node->entry;
         stbuf->st_atime = dir->last_accessed;
         stbuf->st_mtime = dir->last_modified;
         stbuf->st_nlink = 2;
 
-    } else if (((struct lfs_file * )found_struct)->mode == 33279) {
-        struct lfs_file * file = ((struct lfs_file * )found_struct);
-
+    /* Node is a file */
+    } else if (node->mode == 33279){
+        struct lfs_file * file = (struct lfs_file *) node->entry;
         stbuf->st_mode = file->mode;
         stbuf->st_size = file->size;
         stbuf->st_atime = file->last_accessed;
         stbuf->st_mtime = file->last_modified;
         stbuf->st_nlink = 1;
     } else {
-        fprintf(stderr, "getattr: Encountered and unexpected error while trying to find attributes for entry at path '%s'\n", path);
         return -ENOENT;
     }
 
     return res;
-
 }
 
 int lfs_readdir( const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi ) {
@@ -365,18 +182,19 @@ int lfs_readdir( const char *path, void *buf, fuse_fill_dir_t filler, off_t offs
 	filler(buf, ".", NULL, 0);
 	filler(buf, "..", NULL, 0);
 
-    char * abs_path = (char *) malloc(6 + strlen(path));
-    strcpy(abs_path, "root");
-    strcat(abs_path, path);
-
-    struct lfs_directory * dir = find_directory(root_directory, abs_path);
-    free(abs_path);
-
-    if (dir == NULL) {
-        fprintf(stderr, "readdir: Could not find a directory at path '%s' to read\n", path);
+    struct HASHTABLE_NODE * node = find_node_from_path(fs_table, path);
+    
+    /* No node was found with the given path */
+    if (node == NULL) { return -ENOENT; } 
+    
+    /* The node at the given path is not a directory, we we cannot read it as one */
+    if (node->mode != 16877) {
         return -ENOENT;
     }
 
+    /* Extract the lfs_directory struct from the HASHTABLE_NODE 
+    and iterate over its children */
+    struct lfs_directory * dir = (struct lfs_directory *) node->entry;
     for (int i = 0; i < dir->num_directories; i++) {
         filler(buf, dir->directories[i]->name, NULL, 0);
     }
@@ -393,20 +211,17 @@ int lfs_open( const char *path, struct fuse_file_info *fi ) {
 
     printf("open: (path=%s)\n", path);
 
-    char * nonconst_path = strdup(path);
-    char * abs_path = prepend_root(nonconst_path);
-    void * found_struct = find_file_or_directory(abs_path);    
+    struct HASHTABLE_NODE * node = find_node_from_path(fs_table, path);
+    
+    /* No node was found with the given path */
+    if (node == NULL) { return -ENOENT; } 
 
-    free(nonconst_path);
-    free(abs_path);
-
-    /* The file we're trying to open does not exist. */
-    if (found_struct == NULL || ((struct lfs_file *)found_struct)->mode != 33279) {
-        fprintf(stderr, "open: file at path '%s' could not be found\n", path);
+    /* The node at the given path is a directory and can thus not be opened */
+    if (node->mode == 16877) {
         return -ENOENT;
-    } 
+    }
 
-    struct lfs_file * file = (struct lfs_file *) found_struct;
+    struct lfs_file * file = (struct lfs_file *) node->entry;
     file->last_accessed = time(NULL);
     fi->fh = (uint64_t) file;
 
@@ -415,7 +230,6 @@ int lfs_open( const char *path, struct fuse_file_info *fi ) {
 
 int lfs_read( const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi ) {
     printf("read: (path=%s)\n", path);
-     
 
     /* Fetch the pointer to the file, inserted into the
     fuse_file_info struct during lfs_open */
@@ -428,37 +242,24 @@ int lfs_read( const char *path, char *buf, size_t size, off_t offset, struct fus
     memcpy(buf, file->data + offset, bytes_to_read);
     printf("read %d bytes from file '%s'\n", bytes_to_read, path);
     return bytes_to_read;
-      
 }
 
 int lfs_release(const char *path, struct fuse_file_info *fi) {
 	printf("release: (path=%s)\n", path);
-
-    /* Since our filesystem is purely in-memory, and we don't utilize
-    any temporary allocations for interacting with files, we don't need
-    release functionality, so because of that we left this empty. */
-
 	return 0;
 }
 
 int main( int argc, char *argv[] ) {
 
-    /* Setup root directory for our filesystem */
-    root_directory = initialize_directory(NULL, "root");
+    /* Setup Initial Filesystem Table */
+    fs_table = (struct HASHTABLE *) malloc(sizeof(struct HASHTABLE));
+    fs_table->size = STD_FILE_SYSTEM_SIZE; 
 
-    /* Initialization of folders for testing purposes */
-    #ifdef DEBUG
-        struct lfs_directory * home_dir      = initialize_directory(root_directory, "home");
-        struct lfs_directory * fgk_dir       = initialize_directory(home_dir, "fgk");
-        struct lfs_directory * lassan_dir    = initialize_directory(home_dir, "lassan");
-        struct lfs_directory * videos_dir    = initialize_directory(fgk_dir, "videos");
-        struct lfs_directory * downloads_dir = initialize_directory(fgk_dir, "downloads");
-    #endif
-
+    /* Setup Initial Filesystem root directory*/
+    struct lfs_directory * root_directory = initialize_directory(NULL, "/");
+    add_entry_to_table(fs_table, "/", (void *) root_directory);
 
 	fuse_main( argc, argv, &lfs_oper);
-
-
 
 	return 0;
 }
